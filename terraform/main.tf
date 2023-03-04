8d681c44715eb819f90630b276ea075ea1314225
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-03-04 16:57:09 +0000

    adding build index

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,11 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_string" "build_index" {
+  length      = 6
+  min_numeric = 6
+}
+
 resource "azurerm_resource_group" "ai-demo" {
   name     = var.resource_group_name
   location = var.location
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">=3.1.0"
     }
+    random = {
+      source  = "registry.terraform.io/hashicorp/random"
+      version = ">=3.4.0"
+    }
   }
   required_version = ">= 1.0.0"
   backend "azurerm" {}
