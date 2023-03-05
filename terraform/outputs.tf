629012a65372c47f59299f1ea4338d41e650628b
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-03-05 14:42:54 +0000

    adding output for vnet subnet id

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "vmss" {
 output "virtual_network" {
   value = data.azurerm_virtual_network.bootstrap
 }
+
+output "vnet_integration_subnet_id" {
+  value = data.azurerm_subnet.vnet_integration.id
+}
