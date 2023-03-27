c76aff7fefa71e4bc23ffa91670d81c90f6189a8
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-03-27 13:20:46 +0100

    updating depends_on

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -22,8 +22,7 @@ resource "azurerm_key_vault" "application" {
 
   depends_on = [
     azurerm_private_dns_zone.kv,
-    azurerm_private_dns_zone_virtual_network_link.kv,
-    azurerm_private_endpoint.kv
+    azurerm_private_dns_zone_virtual_network_link.kv
   ]
 
   tags = var.tags
